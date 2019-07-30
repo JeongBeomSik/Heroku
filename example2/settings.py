2be17195c437e9f2d7c821e0f80355578f7e886b
JeongBeomSik <cyale344@naver.com>
2019-07-30 16:06:25 +0900

    first commit

--- example2/settings.py
+++ example2/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
